a44982fdfa151f09699f26b9139d692c35f8dd6b
gustavo-cupertino <gcmpires@gmail.com>
2023-06-13 12:37:37 -0300

    app updated

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-# import polars as pl
 import numpy as np
 import pickle
 import sklearn
@@ -16,7 +15,6 @@ def main():
     @st.cache_data(persist=True)
     def load_dataset() -> pd.DataFrame:
         heart_df = pd.read_csv(DATASET_PATH)
-        # heart_df = heart_df.to_pandas()
         heart_df = pd.DataFrame(np.sort(heart_df.values, axis=0),
                                 index=heart_df.index,
                                 columns=heart_df.columns)
